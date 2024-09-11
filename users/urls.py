7c059f70bc1dcdafafa6eb198bc5de9d6b0b366d
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-11 18:48:18 -0300

    created homepage and user auth

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login")
 ]
--- users/views.py
+++ users/views.py
@@ -14,25 +14,16 @@ def register(req):
         email = req.POST.get('email')
         password = req.POST.get('password')
         confirm_password = req.POST.get('confirm-password')
-
-        if not username:
-            messages.add_message(req, constants.ERROR, "Digite o seu nome de usuario")
-            return redirect('/users/register')
-
-        
-        if len(password) < 6:
-            messages.add_message(req, constants.ERROR, "As senhas precisam ter no minimo 6 digitos")
-            return redirect('/users/register')
-        
+ 
         if password != confirm_password:
             messages.add_message(req, constants.ERROR, "As senhas não coincidem")
-            return redirect('/users/register')
+            return redirect('/register')
 
         users = User.objects.filter(username=username)
 
         if users.exists():
             messages.add_message(req, constants.ERROR, "Usuario já cadastrado")
-            return redirect('/users/register')
+            return redirect('/login')
 
         user = User.objects.create_user(
             username=username,
@@ -40,9 +31,7 @@ def register(req):
             password=password
         )
 
-        User.save(user)
-
-        return redirect('/users/login')
+        return redirect('/login')
 
 
 def login(req):
@@ -51,17 +40,16 @@ def login(req):
     elif req.method == 'POST':
         username, email, password = req.POST.get('username'), req.POST.get('email'), req.POST.get('password')
 
-        if not username or not email or not password:
-            messages.add_message(req, constants.ERROR, 'Preencha todos os campos')
-            return redirect('/users/login')
-
         try:
             user = auth.authenticate(req, username=username, email=email, password=password)
             if user:
                 auth.login(req, user)
-                return HttpResponse('home')
-                # return redirect('/') -- HOME
+                return redirect('/')
         except:
+            pass
+        else:
             messages.add_message(req, constants.ERROR, 'Usuário ou senha inválidos')
-            return redirect('/users/login')
-        
+            return redirect('/login')
+
+def home(req):
+    return render(req, 'home.html')
