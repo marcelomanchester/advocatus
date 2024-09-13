8b1c71caf273e5c90a1a713d17039454d1968395
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2024-09-13 17:06:12 -0300

    Tentativa de criação da view da agenda

--- Advocatus/urls.py
+++ Advocatus/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from appointment_calendar.views import calendarPage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('agenda', calendarPage),
 ]
--- appointment_calendar/views.py
+++ appointment_calendar/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def calendarPage(request):
+    return render(request, "appointment_calendar/calendar_page.html")
