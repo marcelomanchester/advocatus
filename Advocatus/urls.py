ec917564e3ca2e7827a8f02dc260a01ffa768e8f
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-14 12:27:48 -0300

    created base process url

--- Advocatus/urls.py
+++ Advocatus/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("processes/", include('processes.urls')),
     path('', include('users.urls')),
 ]
