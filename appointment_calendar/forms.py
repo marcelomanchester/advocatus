79151dcdf0797e02264aafc223e998d55760a08a
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-09-20 14:23:59 -0300

    inserindo componentização do app

--- appointment_calendar/forms.py
+++
@@ -1,8 +0,0 @@
-# appointment_calendar/forms.py
-from django import forms
-from .models import Compromisso
-
-class CompromissoForm(forms.ModelForm):
-    class Meta:
-        model = Compromisso
-        fields = ['titulo', 'descricao', 'data_hora_inicio', 'data_hora_fim']
