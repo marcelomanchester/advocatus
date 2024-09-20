47af82b879267a2d6dee454cfb3e6546b1d877e3
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-09-20 14:23:59 -0300

    inserindo componentização do app

--- Advocatus/urls.py
+++ Advocatus/urls.py
@@ -7,11 +7,5 @@ from appointment_calendar.views import calendarPage
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-<<<<<<< Updated upstream
     path('agenda/', include('appointment_calendar.urls')),
-=======
-    path('agenda', calendarPage),
-    path('adicionar/', views.adicionar_compromisso, name='adicionar_compromisso'),
-    
->>>>>>> Stashed changes
 ]
--- appointment_calendar/views.py
+++ appointment_calendar/views.py
@@ -1,25 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import CompromissoForm
 
-<<<<<<< Updated upstream
+
 def calendarPage(req): # request => req
     user = req.user
 
     if user.is_authenticated: # verificar se o usuario esta autenticado antes de renderizar qualquer pagina
         return render(req, "calendar_page.html")
-=======
-# Create your views here.
-def calendarPage(request):
-    return render(request, "appointment_calendar/calendar_page.html")
-
-def adicionar_compromisso(request):
-    if request.method == 'POST':
-        form = CompromissoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('agenda')  # Substitua por sua URL de agenda
-    else:
-        form = CompromissoForm()
-
-    return render(request, 'appointment_calendar/adicionar_compromisso.html', {'form': form})
->>>>>>> Stashed changes
