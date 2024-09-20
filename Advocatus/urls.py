f85fab8d72b3e6979bc2ff3a8d794ed49cc2d68e
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-09-20 14:11:32 -0300

    Update urls.py

--- Advocatus/urls.py
+++ Advocatus/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from appointment_calendar import views
 
 from appointment_calendar.views import calendarPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+<<<<<<< Updated upstream
     path('agenda/', include('appointment_calendar.urls')),
+=======
+    path('agenda', calendarPage),
+    path('adicionar/', views.adicionar_compromisso, name='adicionar_compromisso'),
+    
+>>>>>>> Stashed changes
 ]
