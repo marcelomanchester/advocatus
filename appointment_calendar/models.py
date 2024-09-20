46008455dd340594daf7a42b671531f36c02b265
BrunofCarvalhoo <168493063+BrunofCarvalhoo@users.noreply.github.com>
2024-09-20 14:11:26 -0300

    Update models.py

--- appointment_calendar/models.py
+++ appointment_calendar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Compromisso(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(null=True, blank=True)
+    data_hora_inicio = models.DateTimeField()
+    data_hora_fim = models.DateTimeField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
