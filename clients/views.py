54714a06c449aa7713b83a1957741eaa09cb811d
Eduardo Henrique <87464701+eduardohasantos@users.noreply.github.com>
2024-09-22 13:09:34 -0300

    merge (processes)

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def home (rec):
-    return render(rec, "client_home.html")
+def home (request):
+    return render(request, "client_home.html")
 
 def register(request):
     return render(request, "client_register.html")
