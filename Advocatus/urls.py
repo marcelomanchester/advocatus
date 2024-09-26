6fb6357fb9c762ab8e8f547ade38bd8be694855e
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-26 13:37:46 -0300

    created finances app

--- Advocatus/urls.py
+++ Advocatus/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("processes/", include('processes.urls')),
     path('', include('users.urls')),
+    path("processes/", include('processes.urls')),
     path('clients/', include('clients.urls')),
     path('agenda/', include('appointment_calendar.urls')),
 ]
