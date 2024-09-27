4092d66291cf3e702e0e67649599502ad65dc782
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-09-27 14:45:32 -0300

    edit and delete process features

--- processes/urls.py
+++ processes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.process, name="processes"),
-    path("process_register", views.register_process, name="process_register")
+    path("process_register", views.register_process, name="process_register"),
+    path('process_edit/<int:id>/', views.edit_process, name='process_edit'),
+    path('process_delete/<int:id>/', views.delete_process, name='process_delete'),
 ]
